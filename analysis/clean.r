cd84f17551e89fa88de2ff3c6a98cd8cd73e29ea
Alex Forrence <alex.forrence@gmail.com>
2024-04-08 16:01:33 -0400

    work out basic data cleaning/analysis

--- analysis/clean.r
+++ analysis/clean.r
@@ -1,6 +1,7 @@
 
 library(data.table)
 library(ggplot2)
+library(glue)
 source('clean_block.r')
 
 # TODO: have data be on a relative path to the analysis
@@ -8,8 +9,19 @@ data_files <- list.files('~/Downloads/covert_attn_data/', glob2rx('msl*.json.gz'
 
 dat <- list()
 
+# TODO: make sure extra data below is written during clean_block instead
+# also include trial/reach start time 
 for (i in 1:length(data_files)) {
+  message(glue('File {i} out of {length(data_files)}: {data_files[i]}'))
   dat[[i]] <- clean_block(data_files[i])
 }
 
-dat <- rbindlist(dat)
+clean_dat <- rbindlist(dat)
+
+reach_dat <- clean_dat[reach_or_probe==1]
+probe_dat <- clean_dat[reach_or_probe==2]
+
+reach_dat[, lag_clamp_side := shift(clamp_side), by=id]
+reach_dat[, delta_angle := endpoint_angle - shift(endpoint_angle), by=id]
+
+# left should be negative delta angle?
--- analysis/clean_block.r
+++ analysis/clean_block.r
@@ -1,5 +1,6 @@
 library(RcppSimdJson)
 library(data.table)
+library(glue)
 
 # in the end, we want two data frames-- one for reach trials, one for probe
 
@@ -19,7 +20,11 @@ signed_diff_angle <- function(a, b) {
 clean_block <- function(filename) {
   raw_dat <- fload(filename, max_simplify_lvl = 'vector', single_null = NA, empty_object = NA, empty_array = NA)
   n_trials <- length(raw_dat$trials$frames)
-  # TODO: check if n_trials is small?
+  if (n_trials < 400) {
+    warning(glue('Skipping {basename(filename)}, only {n_trials} trials...'))
+    return(NULL)
+  }
+
   reach_trials <- list()
   probe_trials <- list()
   
@@ -40,10 +45,14 @@ clean_block <- function(filename) {
   raw_dat$trials$probe <- NULL
   raw_dat$trials$center_px <- NULL
   raw_dat$trials$target_px <- NULL
-  raw_dat$trials$probe_px <- NULL
+  raw_dat$trials$probe_px <- NULL # TODO: store probe location
   
   block_data <- as.data.table(raw_dat$trials)
-  block_data[, `:=`(id = raw_dat$block$id)]
+  block_data[, `:=`(id = raw_dat$block$id,
+                    group = raw_dat$block$group,
+                    trial = 1:.N,
+                    clamp_side = ifelse(clamp_angle==300, 'right', 'left'),
+                    trial_start_time = raw_dat$trials$frames[[1]]$disp_time[1])] # TODO: think about this more
   block_data
 }
 
@@ -58,10 +67,10 @@ compute_reach_angle <- function(reach_data, state_names, origin, target) {
   i <- 1
   while (i <= nrow(foo) - 1) {
     if (!is.na(foo[i+1]$t) && !is.na(foo[i]$t) && foo[i+1]$t - foo[i]$t < 0.001) {
-      foob[[i]] <- data.table(t = foo[i]$t, x = foo[i+1]$x, y = foo[i+1]$y, state = foo[i+1]$state)
+      foob[[i]] <- list(t = foo[i]$t, x = foo[i+1]$x, y = foo[i+1]$y, state = foo[i+1]$state)
       i <- i + 2
     } else {
-      foob[[i]] <- data.table(t = foo[i]$t, x = foo[i]$x, y = foo[i]$y, state = foo[i]$state)
+      foob[[i]] <- list(t = foo[i]$t, x = foo[i]$x, y = foo[i]$y, state = foo[i]$state)
       i <- i + 1
     }
   }
@@ -72,8 +81,8 @@ compute_reach_angle <- function(reach_data, state_names, origin, target) {
   target$x <- target$x - origin$x
   target$y <- target$y - origin$y
   
-  js_data[, angle := rad2deg(atan2(x_px, y_px))]
+  js_data[, angle := rad2deg(atan2(y_px, x_px))]
   target_angle <- rad2deg(atan2(target$y, target$x))
   js_data$diff_angle <- sapply(js_data$angle, signed_diff_angle, b=target_angle)
-  js_data$diff_angle[nrow(js_data)]
+  js_data$diff_angle[nrow(js_data)] # take the last row of js data as the endpoint
 }
